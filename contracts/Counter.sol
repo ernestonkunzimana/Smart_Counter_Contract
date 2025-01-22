dfb53d02befb26668d584a2c8768baf7e7edd848
Ernest Nkunzimana <50280217+ernestonkunzimana@users.noreply.github.com>
2025-01-22 12:50:20 +0200

    Update Counter.sol

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -15,24 +15,3 @@ contract Counter {
         count += 1;
     }
 }
-// File: contracts/Counter.sol (updated)
-
-pragma solidity ^0.8.0;
-
-contract Counter {
-    uint public count;
-    string public description; // We'll add this new variable to store the description
-
-    constructor() {
-        count = 0;
-        description = "This is a counter contract";
-    }
-
-    function increment() public {
-        count += 1;
-    }
-
-    function getDescription() public view returns (string memory) {
-        return description; // Return the current value of 'description'
-    }
-}
